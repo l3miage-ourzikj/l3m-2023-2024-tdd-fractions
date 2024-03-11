beb1e2b301ecb783310ab660cbe7f99e27783eb0
l3miage-ourzikj <Jugurta.Ourzik@etu.univ-grenoble-alpes.fr>
2024-03-11 11:20:21 +0100

    +master: fraction function updated

--- src/app/data/fractions.spec.ts
+++ src/app/data/fractions.spec.ts
@@ -1,4 +1,4 @@
-import { illFormedException, fraction } from "./fractions";
+import { illFormedException, fraction, add } from "./fractions";
 
 
 describe("Instanciations de fractions impossibles", () => {
@@ -55,11 +55,11 @@ describe("Instanciations de fractions possibles", () => {
     it("shouldn't be possible to build the fraction 5 from (-5)/(-1)", () => {
         const f = fraction(-5, -1);
         expect(f.numérateur()).toEqual(5);
-        expect(f.dénominateur()).toEqual(1)
-        expect(f.toString()).toEqual("5")
+        expect(f.dénominateur()).toEqual(1);
+        expect(f.toString()).toEqual("5");
     });
 
-    it("shouldn't be possible to build the fraction -4/3 from (12)/(-9)", () => {
+    it("shouldn  be possible to build the fraction -4/3 from (12)/(-9)", () => {
         const f = fraction(12, -9);
         expect(f.numérateur()).toEqual(-4);
         expect(f.dénominateur()).toEqual(3)
@@ -67,3 +67,26 @@ describe("Instanciations de fractions possibles", () => {
     });
 
 });
+
+
+describe("Addition de deux fractions",()=>{
+
+it("Should be possible to add the two following fractions: 2/5 & 3/10", () => {
+        const f1=fraction(2,5);
+        const f2=fraction(3,10);
+        const resultedFraction=add(f1,f2);
+        expect(() => resultedFraction.numérateur()).toEqual(7);
+        expect(() => resultedFraction.dénominateur()).toEqual(10);
+        expect(() => resultedFraction.toString()).toEqual('7/10');
+
+    });
+  
+
+
+
+
+
+
+
+
+})
--- src/app/data/fractions.ts
+++ src/app/data/fractions.ts
@@ -12,7 +12,7 @@ export interface Fraction {
      * Accesseur pour obtenir le nominateur
      */
     numérateur(): number;
-    
+
     /**
      * Accesseur pour obtenir le dénominateur
      */
@@ -34,18 +34,60 @@ export const illFormedException = new Error("ILL FORMED fraction, should be of t
  * @throws "ILL FORMED fraction, should be of the form Z/Z"
  */
 export function fraction(numérateur: number, dénominateur: number): Fraction {
-    if ( !Number.isInteger(numérateur)
+    if (!Number.isInteger(numérateur)
         || !Number.isInteger(dénominateur)
         || dénominateur === 0
     ) throw illFormedException;
 
-    const s = Math.sign(numérateur) * Math.sign(dénominateur)
-    const n = s * Math.abs(numérateur);
-    const d = Math.abs(dénominateur);
+    let s = Math.sign(numérateur) * Math.sign(dénominateur)
+    let n = s * Math.abs(numérateur);
+    let d = Math.abs(dénominateur);
+
+    const PGCD = pgcd(n, d);
+    n= n/ PGCD;
+    d = d / PGCD;
 
     return {
-        toString:     () => `${n}/${d}`,
-        numérateur:   () => n,
+        toString: () => d != 1 ? `${n}/${d}` : `${n}`,
+        numérateur: () => n,
         dénominateur: () => d
     }
 }
+
+
+/* ------------------- FRACTION UTILS ---------------------- */
+export function add(f1: Fraction, f2: Fraction): Fraction {
+    const n1 = f1.numérateur();
+    const d1 = f1.dénominateur();
+    const n2 = f2.numérateur();
+    const d2 = f2.dénominateur();
+    //calcul du résultat
+    let newNum = n1 * d2 + n2 * d1;
+    let newDenum = d1 * d2;
+
+    //Simplification de la fraction obtenue par division sur le pgcd(newNum,newDenum)
+    const PGCD = pgcd(newNum, newDenum);
+    newNum = newNum / PGCD;
+    newDenum = newDenum / PGCD;
+
+    let s = Math.sign(newNum) * Math.sign(newDenum);
+    newNum = s * Math.abs(newNum)
+    newDenum = s * Math.abs(newDenum);
+
+
+
+    return {
+        toString: () => newDenum != 1 ? `${newNum}/${newDenum}` : `${newNum}`,
+        numérateur: () => newNum,
+        dénominateur: () => newDenum
+    }
+}
+
+function pgcd(a: number, b: number) {
+    if (b) {
+        return pgcd(b, a % b);
+    } else {
+        return Math.abs(a);
+    }
+}
+
